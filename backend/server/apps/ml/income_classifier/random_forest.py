abc54b7a518b39470407f08242c5c3193ea4ccd0
Adam Marek <adam3200@vp.pl>
2020-12-15 22:24:16 +0100

    add tests

--- backend/server/apps/ml/income_classifier/random_forest.py
+++ backend/server/apps/ml/income_classifier/random_forest.py
@@ -3,8 +3,8 @@ import pandas as pd
 
 class RandomForestClassifier:
     def __init__(self):
-        path_to_artifacts = "../../research"
-        self.values_fill_missing = joblib.loan(path_to_artifacts + "train_mode.joblib")
+        path_to_artifacts = "research/"
+        self.values_fill_missing = joblib.load(path_to_artifacts + "train_mode.joblib")
         self.encoders = joblib.load(path_to_artifacts + "encoders.joblib")
         self.model = joblib.load(path_to_artifacts + "random_forest.joblib")
 
