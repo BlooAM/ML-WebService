706b19957a97a90e485a973bc0070a6bb649b07d
Adam Marek <adam3200@vp.pl>
2020-12-26 10:12:32 +0100

    wsgi mod

--- backend/server/server/wsgi.py
+++ backend/server/server/wsgi.py
@@ -8,9 +8,14 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
+import inspect
 
 from django.core.wsgi import get_wsgi_application
 
+from apps.ml.registry import MLRegistry
+from apps.ml.income_classifierier()
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
 
 application = get_wsgi_application()
+
+
