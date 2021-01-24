f331ad24b5b43bccfcdd062aedcd9413f31d9097
Adam Marek <adam3200@vp.pl>
2021-01-24 12:44:39 +0100

    change paths

--- backend/server/server/wsgi.py
+++ backend/server/server/wsgi.py
@@ -1,21 +1,10 @@
-"""
-WSGI config for server project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
-"""
-
 import os
 import inspect
-
 from django.core.wsgi import get_wsgi_application
 
 from apps.ml.registry import MLRegistry
-from apps.ml.income_classifierier()
+from apps.ml.income_classifier.random_forest import RandomForestClassifier
+
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
-
 application = get_wsgi_application()
-
-
